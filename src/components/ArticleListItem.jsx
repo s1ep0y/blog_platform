e400a206d22712dacb78eb32edde8ac4cc7e04af
Danila Molodtsov <danmol04@gmail.com>
2020-06-19 06:28:34 +0300

    new date

--- src/components/ArticleListItem.jsx
+++ src/components/ArticleListItem.jsx
@@ -5,15 +5,28 @@ import {
 import { uniqueId } from 'lodash';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { differenceInMilliseconds, formatDuration } from 'date-fns';
+
+const dateDistance = (date) => {
+    const diff = differenceInMilliseconds(Date.now(), new Date(date))
+    const minutes = Math.floor(diff/60000);
+    const hours = Math.floor(minutes/60)%24;
+    const days = Math.floor(hours/24);
+    return formatDuration({
+        days, hours, minutes
+    },
+    { format: ['days', 'hours', 'minutes']}, { delimiter: '/'})
+} 
 
 const ArticleListItem = (props) => {
     const { title, author, date, tags, likes, likeByUser} = props;
+    dateDistance(date)
     console.log(tags)
     return(
         <div>
         <Card title={title}>
             <p>{author}</p>
-            <p>{date}</p>
+            <p>Posted {dateDistance(date)} ago</p>
             <p>{likes}</p>
             {
                 tags.map((text) =>
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -3,7 +3,7 @@ const baseApi = 'https://conduit.productionready.io/api'
 export default {
     login: () => [baseApi,'users' ,'login'].join('/'),
     users: () => [baseApi, 'users'].join('/'),
-    articles: (quieris = []) => [baseApi, 'articles', ...quieris].join('/'),
+    articles: (quieris = []) => [baseApi, 'articles', ...quieris ].join('/'),
     oneArticle: (name) => [baseApi, 'articles', name].join('/'),
     favArticle: (name) => [baseApi, 'articles', name, 'favorite'],
 
